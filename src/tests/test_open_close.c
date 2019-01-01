c155904de4b7a577bb482de715fc508922d28c95
Andreas Neustifter <andreas.neustifter@kapsch.net>
2019-01-01 14:33:47 +0100

    Add first proper tests.

---
+++ src/tests/test_open_close.c
@@ -0,0 +1,15 @@
+#include <unclog/unclog.h>
+#include <unclog_test.h>
+
+int main(int argc, char** argv) {
+	if (unclog_test_is_initialized() != 0) return -1;
+
+	unclog_t* handle = unclog_open("herbert");
+	if (handle == NULL) return -2;
+	
+	if (unclog_test_is_initialized() == 0) return -3;
+
+	unclog_close(handle);
+	
+	if (unclog_test_is_initialized() != 0) return -4;
+}
