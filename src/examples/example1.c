55974d89b4bd42cb84e93933fe2b459fce8feca5
Andreas Neustifter <andreas.neustifter@kapsch.net>
2019-01-20 22:25:46 +0100

    Update example to put out all logging level messages.

--- src/examples/example1.c
+++ src/examples/example1.c
@@ -1,5 +1,5 @@
 // only one include file is needed for basic usage
-#include <unclog/unclog.h>
+#include <unclog/unclog_adv.h>
 
 int main(int argc, char** argv) {
     (void)argc;
@@ -11,9 +11,13 @@ int main(int argc, char** argv) {
     unclog_t* logger = unclog_open("main");
     // no create some logging messages on several logging levels for this
     // logging source
-    UL_ER(logger, "error message: %d, logger is %p", 42, logger);
-    UL_DE(logger, "debug message: %d, logger is %p", 43, logger);
-    UL_TR(logger, "trace message: %d, logger is %p", 44, logger);
+    UL_FA(logger, "FATAL %d, logger is %p", 42, logger);
+    UL_CR(logger, "CRITICAL %d, logger is %p", 43, logger);
+    UL_ER(logger, "ERROR %d, logger is %p", 44, logger);
+    UL_WA(logger, "WARNING %d, logger is %p", 45, logger);
+    UL_IN(logger, "INFO %d, logger is %p", 46, logger);
+    UL_DE(logger, "DEBUG %d, logger is %p", 47, logger);
+    UL_TR(logger, "TRACE %d, logger is %p", 48, logger);
     // when the logger is not needed anymore it has to be closed. when this was
     // the last source to be closed unclog is de-initialized and all internal
     // memory is freed
